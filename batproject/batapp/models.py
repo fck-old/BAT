64bfd25c86dc9c0efebf317330f4ac2c38e86df9
henning999 <schaaff_he@yahoo.de>
2020-06-05 18:30:18 +0200

    views fuer Bilder hochladen, models und views alles auskommentiert, was sich auf nutzer bezieht

--- batproject/batapp/models.py
+++ batproject/batapp/models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Picture(models.Model):
     picture_path_file = models.ImageField(upload_to='images/')
-    uploaded_by = models.ForeignKey(User, related_name='uploaded')
+    #uploaded_by = models.ForeignKey(User, related_name='uploaded')
--- batproject/batapp/views.py
+++ batproject/batapp/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from batapp.forms import PictureForm
+
+
+def upload_image(request):
+
+    if request.method == 'POST':
+        form = PictureForm(request.POST, request.FILES)
+        if form.is_valid():
+            picture = form.save()
+            # picture.uploaded_by = request.user
+            picture.save()
+            return HttpResponse('upload successful')
+
+        else:
+            form = PictureForm()
+            return render(request, 'image.html', {'form': form})
