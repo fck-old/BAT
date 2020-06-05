dafe047c66a4aef85294160d024dd706752cf4bb
henning999 <schaaff_he@yahoo.de>
2020-06-06 01:12:51 +0200

    weiteres

--- batproject/batproject/settings.py
+++ batproject/batproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'batproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/batapp')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
