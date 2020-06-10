341f39fe949e73f31a185f3e60be9f800b0846a1
James <52463133+JaL11@users.noreply.github.com>
2020-06-10 14:41:11 +0200

    install/deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "getting everything setup..."
+
+#copy deployment files to correct location
+cp docker/Dockerfile docker/docker-compose.yml docker/requirements.txt .
+
+#copy settings.py to project settings
+cp docker/settings.py batproject/batproject/settings.py
+
+echo "starting docker"
+
+docker-compose up
