34897e62a87d00c96b002b48d8e0f09fa191c916
henning999 <schaaff_he@yahoo.de>
2020-06-19 22:46:49 +0200

    Bugs und neues Template zum Testen

--- batproject/batapp/urls.py
+++ batproject/batapp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('getimages/', views.get_all_images, name='getimages'),
     path('', views.functionality, name='functions'),
     path('init/', views.initialise_status_types, name='init'),
-    #path('untagged/', views.get_untagged_picture, name='get_untagged')
+    path('untagged/', views.get_untagged_picture, name='get_untagged')
 ]
 
