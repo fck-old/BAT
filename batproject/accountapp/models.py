3b8818f4058d4c632701090a02a87a83aa29a4ca
CharliesCodeFabrik <vincent-s@posteo.de>
2020-06-19 20:00:52 +0200

    models verändert damit sich die datenbank erneuert

--- batproject/accountapp/models.py
+++ batproject/accountapp/models.py
@@ -9,3 +9,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     last_picture = models.IntegerField(default=-1)
 
+
+class Muell(models.Model):
+    muellzeille = models.IntegerField(default=-1)
+
