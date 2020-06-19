ef47fea4d45096464abbbf6da3798411dd50cf13
henning999 <schaaff_he@yahoo.de>
2020-06-19 16:28:52 +0200

    Label bei Bild hochladen und zurueckgeben hinzugefuegt

--- batproject/batapp/models.py
+++ batproject/batapp/models.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Picture(models.Model):
     picture_path_file = models.ImageField(upload_to='images/')
+    label = models.CharField(max_length=200, default='add label')
     #uploaded_by = models.ForeignKey(User, related_name='uploaded')
