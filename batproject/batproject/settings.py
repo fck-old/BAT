8356379c9b4081824400d22baa28a95cec52512c
Frank Kohlhepp <frank@kohlhepp.me>
2020-07-23 16:08:16 +0200

    Fixed settings to work in dev mode

--- batproject/batproject/settings.py
+++ batproject/batproject/settings.py
@@ -1,8 +1,12 @@
 import os
 
-if os.environ['DJANGO_SETTINGS'] == "prod":
-    print("PROD SERVER")
-    from .settings_prod import *
+if 'DJANGO_SETTINGS' in os.environ:
+    if os.environ['DJANGO_SETTINGS'] == "prod":
+        print("PROD SERVER")
+        from .settings_prod import *
+    else:
+        print("DEV SERVER")
+        from .settings_dev import *
 else:
     print("DEV SERVER")
     from .settings_dev import *
