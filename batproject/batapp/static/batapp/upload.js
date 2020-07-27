e61041b32f600e1a89a1b64ffc6cb243980d4da9
Frank Kohlhepp <frank@kohlhepp.me>
2020-07-27 17:23:34 +0200

    Upload Save Button enable/disable thing

--- batproject/batapp/static/batapp/upload.js
+++ batproject/batapp/static/batapp/upload.js
@@ -4,10 +4,13 @@ $(document).ready(function() {
         document.getElementById("image").src = "";
         $("#file-container").removeClass("d-none");
         $("#id_label").focus();
+        $("#save").attr("disabled", true);
     });
     
     document.getElementById('id_picture_path_file').onchange = function (evt) {
         $("#file-container").addClass("d-none");
+        
+
         
         
         
@@ -31,4 +34,20 @@ $(document).ready(function() {
         
         $("#id_label").focus();
     }
+    
+    
+    
+    function buttonEnabler() {
+        if ($("#id_label").val() != "" && $("#id_picture_path_file").val() != "") {
+            $("#save").attr("disabled", false);
+        } else {
+            $("#save").attr("disabled", true);
+        }
+    }
+    
+    $("#id_label").change(buttonEnabler);
+    $("#id_label").on("keyup", buttonEnabler);
+    $("#id_label").on("keydown", buttonEnabler);
+    $("#id_picture_path_file").change(buttonEnabler);
+    
 });
